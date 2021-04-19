0eb41b31cd53fdce41300803eb8a0b4cd1d75181
ChrisK-0 <73487474+ChrisK-0@users.noreply.github.com>
2021-04-19 15:46:54 +0300

    JS patch
    * Updated modal: Made the display: none into a function called closeModal() and added pressing Escape key to modal closing

--- accordion.js
+++ accordion.js
@@ -76,28 +76,37 @@ function gotoAccord() {
 
 
 // get the modal
-var getModal = document.getElementById("policyModal");
-
-// get the button that opens the modal
+var modal = document.getElementById("policyModal");
+// get the anchor text that opens the modal
 var openModal = document.getElementById("policyOpen");
-
 // get the <span> element that closes the modal
 var modalSpan = document.getElementsByClassName("closeModal")[0];
+// modal closing function
+function closeModal() {
+  modal.style.display = "none";
+};
 
-// when the user clicks on the button, open the modal
+
+// when the user clicks on the anchor text, open the modal
 openModal.onclick = function () {
-  getModal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // when the user clicks on <span> (x), close the modal
-modalSpan.onclick = function () {
-  getModal.style.display = "none";
-}
+modalSpan.onclick = closeModal;
 
-// when the user clicks anywhere outside of the modal, close it
+// when the user clicks ESC on their keyboard, close the modal
+window.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' ) {
+    closeModal();
+  }
+});
+
+// when the user clicks anywhere outside of the modal, close it (also added ESC key)
 window.onclick = function (event) {
-  if (event.target == getModal) {
-    getModal.style.display = "none";
+  if (event.target == modal) {
+    closeModal();
+
   }
 }
 
